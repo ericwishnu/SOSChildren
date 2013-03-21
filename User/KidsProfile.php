631994745a8ac0066dfd7d02b1fc583d72eeec43
Shan Markus <shanmarkus@gmail.com>
2013-03-21 23:43:42 +0700

    fixing kidsProfile.php

--- User/KidsProfile.php
+++ User/KidsProfile.php
@@ -29,196 +29,191 @@ for ($i = 0; $i < count($foundationname); $i++) {
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="../css/mainMenuCss.css">
-        <link rel="stylesheet" type="text/css" href="../css/kidsProfile.css">
-        <title>Kids Profile</title>
-    </head>
-    <body class="Background">
-          <div class="Header" >
-            <!-- Header class -->
-            <div class="row">
-                <div class="span12 margin-leftHeader margin-topHeader">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/mainMenuCss.css">
+    <link rel="stylesheet" type="text/css" href="../css/kidsProfile.css">
+    <title>Kids Profile</title>
+</head>
+<body class="Background">
+  <div class="Header" >
+    <!-- Header class -->
+    <div class="row">
+        <div class="span12 margin-leftHeader margin-topHeader">
 
-                    <div class="row-fluid">
-                        <div class="offset7 span4 margin-rightHeader">
-                            <?php include 'GlobalSearch.php' ?>
-                        </div>
+            <div class="row-fluid">
+                <div class="offset7 span4 margin-rightHeader">
+                    <?php include 'GlobalSearch.php' ?>
+                </div>
 
-                        <div class="span1">
-                            <?php include 'UserDropdownMenu.php'; ?>
-                        </div>
-                    </div>
-
+                <div class="span1">
+                    <?php include 'UserDropdownMenu.php'; ?>
                 </div>
             </div>
 
         </div>
+    </div>
 
-        <div class="row">
-            <div class="span3 Logo">
-                <!-- Logo Div  -->
+</div>
+
+<div class="row">
+    <div class="span3 Logo">
+        <!-- Logo Div  -->
+    </div>
+</div>
+
+<div class="row">
+
+    <div class="container">
+        <div class="row-fluid">
+            <div class="span2 paddingLeft">
+
+                <?php include('UserNavigation.php'); ?>
+
             </div>
-        </div>
 
-        <div class="row">
 
-            <div class="container">
+            <div class="span9">
                 <div class="row-fluid">
-                    <div class="span2 paddingLeft">
-
-                        <?php include('UserNavigation.php'); ?>
+                    <div class="kidsProfilePicture span2">
+                        <img width="100%" style="border-radius:10px" src="../Database/Images/Kids/<? echo $photo ?>" alt="<? $kidsData->getName() ?>"/>
 
                     </div>
 
-
-                    <div class="span9">
+                    <div class="kidsProfilePost span9">
                         <div class="row-fluid">
-                            <div class="kidsProfilePicture span2">
-                                <img width="100%" style="border-radius:10px" src="../Database/Images/Kids/<? echo $photo ?>" alt="<? $kidsData->getName() ?>"/>
 
-                            </div>
+                            <div class="span11 offset1">
+                                <h1><? echo $name ?></h1>
 
-                            <div class="kidsProfilePost span9">
-                                <div class="row-fluid">
+                                <form name="foundation" action="FoundationCtrl.php" method="post">
+                                    <input type="hidden" name="action" value="foundationprofile"/>
+                                    <input type="hidden" name="foundationSelectedID" value="<?php echo $foundationID?>"/>
 
-                                    <div class="span11 offset1">
-                                        <h1><? echo $name ?></h1>
-                                        
-                                        <form name="foundation" action="FoundationCtrl.php" method="post">
-                                            <input type="hidden" name="action" value="foundationprofile"/>
-                                            <input type="hidden" name="foundationSelectedID" value="<?php echo $foundationID?>"/>
-                                             
-                                        </form>
-                                       <a href="javascript: foundation()"><? echo $foundation ?></a> 
-                                        <script>
-                                            function foundation(){
-                                                document.foundation.submit();
-                                            }
-                                            </script>
-                                                <?php echo " - " .$kidsData->getRegion() ?><br/>
-                                        Aspiration <? echo $aspiration ?>
-                                    </div>	
+                                </form>
+                                <a href="javascript: foundation()"><? echo $foundation ?></a> 
+                                <script>
+                                function foundation(){
+                                    document.foundation.submit();
+                                }
+                                </script>
+                                <?php echo " - " .$kidsData->getRegion() ?><br/>
+                                Aspiration <? echo $aspiration ?>
+                            </div>  
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="row-fluid margin-top">
+                    <!-- Main Menu Class -->
+                    <div class="span12">
+
+                        <div class="row-fluid">
+                            <div class="kidsFeedTop">
+                                <div class="myFont">
+                                    <p class="myFont"> Information </p>
                                 </div>
                             </div>
-
                         </div>
 
-                        <div class="row-fluid margin-top">
-                            <!-- Main Menu Class -->
-                            <div class="span12">
-
-                                <div class="row-fluid">
-                                    <div class="kidsFeedTop">
-                                        <div class="myFont">
-                                            <p class="myFont"> Information </p>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row-fluid">
-                                    <div class="kidsFeedMiddle" >
-                                        <table>
-<!--                                            <tr>
+                        <div class="row-fluid">
+                            <div class="kidsFeedMiddle" >
+                                <table>
+       <!--                                      <tr>
                                                 <td>Foundation ID</td>
                                                 <td>:</td>
-                                                <td><? //echo $kidsData->getFoundationID() ?></td>
-                                            </tr>-->
+                                                <td><? echo $kidsData->getFoundationID() ?></td>
+                                            </tr> -->
 
                                             <tr>
-                                                <td>DOB</td>
-                                                <td>:</td>
-                                                <td><? echo $kidsData->getDOB() ?></td>
+                                                <td><img src="../img/kidsinfo/kidsicon_dob.png" alt="Smiley face" height="42" width="42"> </td>
+                                                <td> &nbsp; <? echo $kidsData->getDOB() ?></td>
                                             </tr>
                                             
                                             <tr>
-                                                <td>Age</td>
-                                                <td>:</td>
-                                                <td><?php echo $age?> years old</td>
+                                                <td><img src="../img/kidsinfo/kidsicon_age.png" alt="Smiley face" height="42" width="42"></td>
+                                                <td> &nbsp; <?php echo $age?> years old</td>
                                             </tr>
 
                                             <tr>
-                                                <td>Origin</td>
-                                                <td>:</td>
-                                                <td><? echo $kidsData->getOrigin() ?></td>
-                                            </tr>
+                                             <td><img src="../img/kidsinfo/kidsicon_origin.png" alt="Smiley face" height="42" width="42"></td>
+                                             <td> &nbsp; <? echo $kidsData->getOrigin() ?></td>
+                                         </tr>
 
-                                            <tr>
+                                         <tr>
+                                            <td><img src="../img/kidsinfo/kidsicon_parentsjob.png" alt="Smiley face" height="42" width="42"></td>
+                                            <td valign="center"> &nbsp; <? echo $kidsData->getBackground() ?></td>
+                                        </tr>
+                                        <tr>
 
-                                                <td valign="top">Background</td>
-                                                <td valign="top">:</td>
-                                                <td valign="top"><? echo $kidsData->getBackground() ?></td>
-                                            </tr>
-                                            <tr>
+                                           <td><img src="../img/kidsinfo/kidsicon_needs.png" alt="Smiley face" height="42" width="42"></td>
+                                           <td valign="top">
+                                            <?
+                                            if ($kidsData->getHealth() == '1') {
+                                                ?>  &nbsp; 
+                                                Health <br>
+                                                <?
+                                            }
+                                            if ($kidsData->getEducation() == '1') {
+                                                ?>  &nbsp; 
+                                                Education <br>
+                                                <?
+                                            }
+                                            if ($kidsData->getNutrition() == '1') {
+                                                ?>  &nbsp; 
+                                                Nutrition
+                                                <?
+                                            }
+                                            ?> 
+                                        </td>
+                                    </tr>
+                                </table>               
 
-                                                <td valign="top">Needs</td>
-                                                <td valign="top">:</td>
-                                                <td valign="top">
-                                                    <?
-                                                    if ($kidsData->getHealth() == '1') {
+
+
+                                <form action='KidsCtrl.php' method="post">
+                                    <input type="hidden" name="action" value="prepareFosterKids">
+
+                                    <table class="margin-top"><!-- TABLE AFTER FIRST TABLE -->
+
+                                        <tr>
+                                            <td valign="left"  halign="bottom" frame="box" rowspan="7" width="200px">
+
+                                                <?
+                                                if (isset($_SESSION['usernameU']) && $_SESSION['usernameU'] != "") {
+                                                    if (isset($_SESSION['checkFoster']) && $_SESSION['checkFoster'] == "false") {
                                                         ?>
-                                                        Health <br>
+                                                        <input type="submit" value="Foster Me" class="myFont-button">
+
                                                         <?
                                                     }
-                                                    if ($kidsData->getEducation() == '1') {
-                                                        ?>
-                                                        Education <br>
-                                                        <?
-                                                    }
-                                                    if ($kidsData->getNutrition() == '1') {
-                                                        ?>
-                                                        Nutrition
-                                                        <?
-                                                    }
-                                                    ?> 
-                                                </td>
-                                            </tr>
-                                        </table>               
+                                                }
+                                                ?>
+                                                <input type="hidden" name="kidsSelectedID" value="<? echo $kidsData->getKidsID() ?>">
+
+                                                <input type="hidden" name="kidsID" value="<? echo $kidsData->getKidsID() ?>">
+                                            </td>
+                                        </tr>
+                                    </table><!-- END OF TABLE AFTER FIRST TABLE -->
+                                </form>
 
 
-
-                                        <form action='KidsCtrl.php' method="post">
-                                            <input type="hidden" name="action" value="prepareFosterKids">
-
-                                            <table><!-- TABLE AFTER FIRST TABLE -->
-                                                <tr>
-                                                    <td valign="top" frame="box"align="center" rowspan="7" width="200px">
-
-                                                        <?
-                                                        if (isset($_SESSION['usernameU']) && $_SESSION['usernameU'] != "") {
-                                                            if (isset($_SESSION['checkFoster']) && $_SESSION['checkFoster'] == "false") {
-                                                                ?>
-                                                                <input type="submit" value="Foster Me" class="myFont-button">
-
-                                                                <?
-                                                            }
-                                                        }
-                                                        ?>
-                                                        <input type="hidden" name="kidsSelectedID" value="<? echo $kidsData->getKidsID() ?>">
-
-                                                        <input type="hidden" name="kidsID" value="<? echo $kidsData->getKidsID() ?>">
-                                                    </td>
-                                                </tr>
-                                            </table><!-- END OF TABLE AFTER FIRST TABLE -->
-                                        </form>
-
-
-                                        <div class="row">
-                                            <div class="accordion margin-top" id="accordion2">
-                                                <div class="accordion-group">
-                                                    <div class="accordion-heading">
-                                                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
-                                                            Kid's Journal
-                                                        </a>
-                                                    </div>
-                                                </div>
+                                <div class="row">
+                                    <div class="accordion margin-top" id="accordion2">
+                                        <div class="accordion-group">
+                                            <div class="accordion-heading">
+                                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+                                                    Kid's Journal
+                                                </a>
                                             </div>
+                                        </div>
+                                    </div>
 
-                                            <div id="collapseOne" class="accordion-body collapse">
-                                                <div class="accordion-inner">
-                                                    Coming Soon . . .
+                                    <div id="collapseOne" class="accordion-body collapse">
+                                        <div class="accordion-inner">
+                                            Coming Soon . . .
 <!--                                                    "Lorem ipsum dolor sit amet, consectetur adipisicing elit,
                                                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -272,7 +267,7 @@ for ($i = 0; $i < count($foundationname); $i++) {
 
             <div class="Footer margin-top">
                 <!-- footer div -->
-             <div id="footer" style="float:right; margin:5px">
+                <div id="footer" style="float:right; margin:5px">
                     Copyright © Crying Onion 2013
                 </div>
             </div>
@@ -282,5 +277,5 @@ for ($i = 0; $i < count($foundationname); $i++) {
             <script src="../js/bootstrap-modal.js"></script>  
             <script src="../js/js-script.js"></script>
             <script src="../js/bootstrap.min.js"></script>
-    </body>
-</html>
+        </body>
+        </html>
