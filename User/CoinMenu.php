7c80c1d2525d44cae576d60a076f2304bd11bf2d
Eric Wishnu Saputra <eric.wishnu@hotmail.com>
2013-03-22 23:57:06 +0700

    CoinsMenu  Short one

--- User/CoinMenu.php
+++ User/CoinMenu.php
@@ -7,220 +7,127 @@ include 'User.php';
 
 <html>
 
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="../css/mainMenuCss.css">
-	<link rel="stylesheet" type="text/css" href="../css/coinMenu.css">
-	<title>Coins.. Coins Everywhere</title>
-</head>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../css/mainMenuCss.css">
+        <link rel="stylesheet" type="text/css" href="../css/coinMenu.css">
+        <title>Coins.. Coins Everywhere</title>
+    </head>
 
-<body class="Background">
+    <body class="Background">
 
-	<div class="Header">
-		<!-- Header class -->
-	</div>
+        <?php include 'UserHeader.php' ?>
 
-	<div class="row">
-		<div class="span3 Logo">
-			<!-- Logo Div  -->
-		</div>
-	</div>
+        <div class="row">
+            <div class="span3 Logo">
+                <!-- Logo Div  -->
+            </div>
+        </div>
 
-	<div class="row">
+        <div class="row">
 
-		<div class="container">
-			<div class="row-fluid">
+            <div class="container">
+                <div class="row-fluid">
 
-				<div class="span2 paddingLeft">
+                    <div class="span2 paddingLeft">
 
-					<?php include 'UserNavigation.php'; ?>
+                        <?php include 'UserNavigation.php'; ?>
 
-				</div>
+                    </div>
 
 
 
-				<div class="span9">
+                    <div class="span9">
 
-					<!-- USER POST DIVISION -->
-					 <?php include 'PostStatus.php'; ?>
+                        <!-- USER POST DIVISION -->
+                        <?php include 'PostStatus.php'; ?>
 
-				<div class="row-fluid margin-top">
-					<!-- Main Menu Class -->
-					<div class="span12">
+                        <div class="row-fluid margin-top">
+                            <!-- Main Menu Class -->
+                            <div class="span12">
 
-						<div class="row-fluid">
-							<div class="coinsFeedTop">
-								<div class="myFont">
-									<p class="myFont"> Coins </p>
-								</div>
-							</div>
-						</div>
+                                <div class="row-fluid">
+                                    <div class="coinsFeedTop">
+                                        <div class="myFont">
+                                            <p class="myFont"> Coins </p>
+                                        </div>
+                                    </div>
+                                </div>
 
-						<div class="row-fluid">
-							<div class="coinsFeedMiddle">
-								<div class="row">
-									<div class="row">
-										<div class="span4 offset1 coinHeader">
-											<div class="span4 underlogo"></div>
-										</div>
-										<div class="span4 offset2">
-											<form action="UserCtrl.php" method="post">
-												<input type="hidden" name="action" value="showmycoin"/>
-												<input type="submit" class="span12 checkCoin" value="">
-											</form>
-											<div class="row">
-												<div class="textAmount span12 offset1"></div> 
-											</div>
-											<div class="row input-prepend">
-												<form>
-													<input type="text" class="offset4 input-small" readonly value=""><span class="add-on">Coins</span>
-												</form>
-											</div>
-										</div>
-									</div>	
-								</div>
+                                <div class="row-fluid">
+                                    <div class="coinsFeedMiddle">
+                                        <div class="row">
+                                            <div class="row">
+                                                <div class="span4 offset1 coinHeader">
+                                                    <div class="span4 underlogo"></div>
+                                                </div>
+                                                <div class="span4 offset2">
+                                                    <form action="UserCtrl.php" method="post">
+                                                        <input type="hidden" name="action" value="showmycoin"/>
+                                                        <input type="submit" class="span12 checkCoin" value="">
+                                                    </form>
+                                                    <div class="row">
+                                                        <div class="textAmount span12 offset1"></div> 
+                                                    </div>
+                                                    <div class="row input-prepend">
+                                                        <form>
+                                                            <input type="text" class="offset4 input-small" readonly value=""><span class="add-on">Coins</span>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>	
+                                        </div>
 
-								<div class="row">
-									<div class="span4 offset1">
-										<div class="row">
-											<div class="textBuy span10" value="">
-											</div>
-										</div>
+                                        <div class="row">
+                                            <div class="span4 offset1">
+                                                <div class="row">
+                                                    <div class="textBuy span10" value="">
+                                                    </div>
+                                                </div>
 
-										<div class="row span12 input-prepend">
-											<form class="margin-top">
-												<input type="text" class="input-small" value="">
-												<span class="add-on">Coins</span>
-											</form>
+                                                <div class="row span12 input-prepend">
+                                                    <form class="margin-top">
+                                                        <input type="text" class="input-small" value="">
+                                                        <span class="add-on">Coins</span>
+                                                    </form>
 
-										</div>
-										<div class="row">
-											<form>
-												<input type="submit" class="buyCoin span10" value="">
-											</form>
-										</div>
-									</div>
+                                                </div>
+                                                <div class="row">
+                                                    <form>
+                                                        <input type="submit" class="buyCoin span10" value="">
+                                                    </form>
+                                                </div>
+                                            </div>
 
-									<div class="offset1 span5">
-										<div class="bigCoin"></div>
-									</div>
-								</div>
-							</div>
+                                            <div class="offset1 span5">
+                                                <div class="bigCoin"></div>
+                                            </div>
+                                        </div>
+                                    </div>
 
-							<div class="row-fluid">
-								<div class="coinsFeedBot">
-								</div>
-							</div>
-						</div>
+                                    <div class="row-fluid">
+                                        <div class="coinsFeedBot">
+                                        </div>
+                                    </div>
+                                </div>
 
-					</div>
-				</div>
-			</div>
+                            </div>
+                        </div>
+                    </div>
 
-			<div class="span1">
+                    <?php include 'UserModal.php' ?>
 
-				<div class="row-fluid">
-					<form>
-						<input type="button" class="span12 pickMeButton" id="pickme" name="pickme" data-toggle="modal" href="#pickMeModal">
-					</form>
-				</div>
-
-				<div class="row-fluid">
-					<form>
-						<input type="button" class="span12 recommendedButton" id="recommended" name="recommended" data-toggle="modal" href="#Recomended">
-					</form>
-				</div>
-
-				<div class="row-fluid">
-					<form>
-						<input type="button" class="span12 emergencyButton" id="emergency" name="emergency" data-toggle="modal" href="#Emergency">
-					</form>
-				</div>
-
-				<div class="row-fluid marginfriendButton">
-					<form>
-						<input type="submit" value="" class="span12 friendButton" id="emergency" name="emergency">
-					</form>
-				</div>
-
-			</div>
-
-		</div>
-	</div>
+                </div>
+            </div>
 
 
-	<div class="Footer margin-top ">
-		<!-- footer div -->
-		<div id="footer" style="float:right; margin:5px">
-			Copyright © Crying Onion 2013
-		</div>
-	</div>
+            <?php include 'Footer.php' ?>
+            <?php include 'Script.php' ?>
 
 
-<!-- Modal for Widget button 
--->
-
-<!-- Pick Me Modal Division -->
-
-<div id="pickMeModal" class="modal hide fade in" style="display: none; ">  
-	<div class="modal-header modalPink modal-radius">  
-		<a class="close" data-dismiss="modal">×</a>  
-		<h3>This is a Pick Me Modal Example</h3>  
-	</div>  
-	<div class="modal-body">  
-		<h4>Text in a modal</h4>  
-		<p>You can add some text here.</p>                
-	</div>  
-	<div class="modal-footer modalPink">  
-		<a href="#" class="btn btn-success">Call to action</a>  
-		<a href="#" class="btn" data-dismiss="modal">Close</a>  
-	</div>  
-</div>  
-
-<!-- 
-	Recommended Modal Division -->
-
-	<div id="Recomended" class="modal hide fade in" style="display: none; ">  
-		<div class="modal-header modalPink modal-radius">  
-			<a class="close" data-dismiss="modal">×</a>  
-			<h3>Recommended</h3>  
-		</div>  
-		<div class="modal-body">  
-			<h4>Text in a modal</h4>  
-			<p>You can add some text here.</p>                
-		</div>  
-		<div class="modal-footer modalPink">  
-			<a href="#" class="btn btn-success">Call to action</a>  
-			<a href="#" class="btn" data-dismiss="modal">Close</a>  
-		</div>  
-	</div>  
 
 
-	<!-- Emergency Button -->
-
-	<div id="Emergency" class="modal hide fade in" style="display: none;">
-
-		<div class="modal-header modalPink modal-radius">  
-			<a class="close" data-dismiss="modal">×</a>  
-			<h2>Emergency</h2>  
-		</div>  
-		<div class="modal-body">  
-			<h4>Text in a modal</h4>  
-			<p>Add some Text Here</p>
-		</div>  
-		<div class="modal-footer modalPink">  
-			<a href="#" class="btn btn-success">Call to action</a>  
-			<a href="#" class="btn" data-dismiss="modal">Close</a>  
-		</div>  
-
-	</div>  
-
-	<!-- SCRIPT !!!  -->
-
-	<script src="../js/jquery.js"></script>  
-	<script src="../js/bootstrap-modal.js"></script>  
-
-</body>
+    </body>
 
 </html>
