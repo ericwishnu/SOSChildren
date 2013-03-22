8bfef47b6e39d1c154a45e4b7b202d56bfcec12d
Eric Wishnu Saputra <eric.wishnu@hotmail.com>
2013-03-23 04:47:57 +0700

    redirect

--- index.php
+++ index.php
@@ -3,8 +3,19 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>SOS Children</title>
+        <script >
+        function userLogin(){
+            if(window.location.hostname=="localhost"){
+            window.location = "http://"+window.location.hostname+"/SOSChildren/User/UserLogin.php";
+        }
+        else{
+           window.location=  "http://soschildren.azurewebsites.net/User/UserLogin.php";
+        }
+        
+        }
+        </script>
     </head>
-    <body>
+    <body onload="userLogin()">
         <a href="Admin/AdminLogin.php">Admin Login</a><br>
         <a href="Foundation/FoundationLogin.php">Foundation Login</a><br>
         <a href="User/UserLogin.php">User Login</a>
