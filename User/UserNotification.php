1d475f660d4822d654d3c029f19ac2a669f7007c
Shan Markus <shanmarkus@gmail.com>
2013-03-22 19:47:34 +0700

    Fixing Friends widget

--- User/UserNotification.php
+++ User/UserNotification.php
@@ -5,75 +5,75 @@ include 'User.php';
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="../css/mainMenuCss.css">
-        <title>Here's Some Heads Up</title>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/mainMenuCss.css">
+    <title>Here's Some Heads Up</title>
 
-    </head>
-    <body class="Background">
-        <div class="Header" >
-            <!-- Header class -->
-            <div class="row">
-                <div class="span12 margin-leftHeader margin-topHeader">
+</head>
+<body class="Background">
+    <div class="Header" >
+        <!-- Header class -->
+        <div class="row">
+            <div class="span12 margin-leftHeader margin-topHeader">
 
-                    <div class="row-fluid">
-                        <div class="offset7 span4 margin-rightHeader">
-                            <?php include 'GlobalSearch.php' ?>
-                        </div>
-
-                        <div class="span1">
-                            <?php include 'UserDropdownMenu.php'; ?>
-                        </div>
+                <div class="row-fluid">
+                    <div class="offset7 span4 margin-rightHeader">
+                        <?php include 'GlobalSearch.php' ?>
                     </div>
+
+                    <div class="span1">
+                        <?php include 'UserDropdownMenu.php'; ?>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row">
+        <div class="span3 Logo">
+            <!-- Logo Div  -->
+        </div>
+    </div>
+
+    <div class="row">
+
+        <div class="container">
+            <div class="row-fluid">
+                <div class="span2 paddingLeft">
+
+                    <?php include('UserNavigation.php'); ?>
 
                 </div>
-            </div>
-
-        </div>
-
-        <div class="row">
-            <div class="span3 Logo">
-                <!-- Logo Div  -->
-            </div>
-        </div>
-
-        <div class="row">
-
-            <div class="container">
-                <div class="row-fluid">
-                    <div class="span2 paddingLeft">
-
-                        <?php include('UserNavigation.php'); ?>
-
-                    </div>
 
 
-                    <div class="span9">
+                <div class="span9">
 
-                        <?php include 'PostStatus.php'; ?>
+                    <?php include 'PostStatus.php'; ?>
 
-                        <div class="row-fluid margin-top">
-                            <!-- Main Menu Class -->
-                            <div class="span12">
+                    <div class="row-fluid margin-top">
+                        <!-- Main Menu Class -->
+                        <div class="span12">
 
-                                <div class="row-fluid">
-                                    <div class="notifFeedTop">
-                                        <div class="myFont">
-                                            <p class="myFont"> Notification </p>
-                                        </div>
+                            <div class="row-fluid">
+                                <div class="notifFeedTop">
+                                    <div class="myFont">
+                                        <p class="myFont"> Notification </p>
                                     </div>
                                 </div>
+                            </div>
 
-                                <div class="row-fluid">
-                                    <div class="notifFeedMiddle">
+                            <div class="row-fluid">
+                                <div class="notifFeedMiddle">
 
-                                        <?php
-                                        $peopleList = unserialize($_SESSION['peoplelistdataobj']);
+                                    <?php
+                                    $peopleList = unserialize($_SESSION['peoplelistdataobj']);
 
-                                        if (count($peopleList) > 0) {
-                                            ?>
+                                    if (count($peopleList) > 0) {
+                                        ?>
 
 
 <!--
@@ -116,64 +116,64 @@ include 'User.php';
                                                     $coins = $temp->getCoins();
                                                     ?>
 
-                                                
-                                                
-                                                        <div style="float:left" >
-                                                            <div>
-                                                                <img  style="width:150px;margin:15px; height:150px; float:left" src="<? echo "../Database/Images/Sponsor/" . $photo; ?>" />
-                                                                <form name="sponsorprofile<?php echo md5($i) ?>" action="PeopleCtrl.php" method="post">
-                                                                    <input type="hidden" name="action" value="seepeopleprofile">
-                                                                    <input type="hidden" name="peopleSelectedID" value="<? echo $peopleID ?>">
+                                                    
+                                                    
+                                                    <div style="float:left" >
+                                                        <div>
+                                                            <img  style="width:150px;margin:15px; height:150px; float:left" src="<? echo "../Database/Images/Sponsor/" . $photo; ?>" />
+                                                            <form name="sponsorprofile<?php echo md5($i) ?>" action="PeopleCtrl.php" method="post">
+                                                                <input type="hidden" name="action" value="seepeopleprofile">
+                                                                <input type="hidden" name="peopleSelectedID" value="<? echo $peopleID ?>">
 
-                                                                </form>
-                                                            </div>
-
-                                                            <div  >
-                                                                <p style="padding:25px; text-align:left; position: relative; vertical-align: middle; width:400px" >
-                                                                    <a href="javascript: sponsorprofile<?php echo md5($i) ?>()"><font size="5"><?php echo $name; ?></font></br></a>
-                                                                    <script>
-                                                                        function sponsorprofile<?php echo md5($i) ?>() {
-                                                                            document.sponsorprofile<?php echo md5($i) ?>.submit();
-                                                                        }
-                                                                    </script>
-                                                                    <?php echo $email; ?></br>
-                                                                    <?php if ($city != "") echo $city . "</br>"; ?>
-                                                                    <?php if ($state != "") echo $state . "</br>"; ?>
-                                                                    <?php if ($country != "") echo $country . "</br>"; ?>
-                                                                    <?php if ($phone != "") echo $phone . "</br>"; ?>
-
-                                                                </p>
-                                                            </div>
-                                                            <div style="float:right">
-                                                                        
-                                                                  
-                                                               
-                                                              
-                                                                   <form action="PeopleCtrl.php" method="post">
-                                                                       <input type="hidden" name="action" value="approveneighbour">
-                                                                       <input type="hidden" name="peopleID" value="<?// echo $peopleID ?>">
-                                                                       <input type="submit" value="Approve">
-                                                                   </form>
-                                                                   <form action="PeopleCtrl.php" method="post">
-                                                                       <input type="hidden" name="action" value="removeneighbour">
-                                                                       <input type="hidden" name="peopleID" value="<?// echo $peopleID ?>">
-                                                                       <input type="submit" value="Decline">
-                                                                   </form>
-                                                              
-                                                            </div>
+                                                            </form>
                                                         </div>
-                                                        <div style="clear:both">
 
+                                                        <div  >
+                                                            <p style="padding:25px; text-align:left; position: relative; vertical-align: middle; width:400px" >
+                                                                <a href="javascript: sponsorprofile<?php echo md5($i) ?>()"><font size="5"><?php echo $name; ?></font></br></a>
+                                                                <script>
+                                                                function sponsorprofile<?php echo md5($i) ?>() {
+                                                                    document.sponsorprofile<?php echo md5($i) ?>.submit();
+                                                                }
+                                                                </script>
+                                                                <?php echo $email; ?></br>
+                                                                <?php if ($city != "") echo $city . "</br>"; ?>
+                                                                <?php if ($state != "") echo $state . "</br>"; ?>
+                                                                <?php if ($country != "") echo $country . "</br>"; ?>
+                                                                <?php if ($phone != "") echo $phone . "</br>"; ?>
+
+                                                            </p>
                                                         </div>
-                                                
-                                                
+                                                        <div style="float:right">
+                                                            
+                                                          
+                                                         
+                                                          
+                                                         <form action="PeopleCtrl.php" method="post">
+                                                             <input type="hidden" name="action" value="approveneighbour">
+                                                             <input type="hidden" name="peopleID" value="<?// echo $peopleID ?>">
+                                                             <input type="submit" value="Approve">
+                                                         </form>
+                                                         <form action="PeopleCtrl.php" method="post">
+                                                             <input type="hidden" name="action" value="removeneighbour">
+                                                             <input type="hidden" name="peopleID" value="<?// echo $peopleID ?>">
+                                                             <input type="submit" value="Decline">
+                                                         </form>
+                                                         
+                                                     </div>
+                                                 </div>
+                                                 <div style="clear:both">
 
-                                                    <?
-                                                }
-                                                ?>
-                                            
-                                            <?
-                                        } else {
+                                                 </div>
+                                                 
+                                                 
+
+                                                 <?
+                                             }
+                                             ?>
+                                             
+                                             <?
+                                         } else {
                                             echo 'Not Avaiable';
                                         }
                                         ?>
@@ -210,6 +210,12 @@ include 'User.php';
                             </form>
                         </div>
 
+                        <div class="row-fluid marginfriendButton">
+                            <form>
+                                <input type="submit" value="" class="span12 friendButton" id="emergency" name="emergency">
+                            </form>
+                        </div>
+
                     </div>
 
                 </div>
@@ -229,27 +235,27 @@ include 'User.php';
 
 
             <!-- Modal for Widget button 
-            -->
+        -->
 
-            <!-- Pick Me Modal Division -->
+        <!-- Pick Me Modal Division -->
 
-            <div id="pickMeModal" class="modal hide fade in" style="display: none; ">  
-                <div class="modal-header modalPink modal-radius">  
-                    <a class="close" data-dismiss="modal">×</a>  
-                    <h3>This is a Pick Me Modal Example</h3>  
-                </div>  
-                <div class="modal-body">  
-                    <h4>Text in a modal</h4>  
-                    <p>You can add some text here.</p>                
-                </div>  
-                <div class="modal-footer modalPink">  
-                    <a href="#" class="btn btn-success">Call to action</a>  
-                    <a href="#" class="btn" data-dismiss="modal">Close</a>  
-                </div>  
+        <div id="pickMeModal" class="modal hide fade in" style="display: none; ">  
+            <div class="modal-header modalPink modal-radius">  
+                <a class="close" data-dismiss="modal">×</a>  
+                <h3>This is a Pick Me Modal Example</h3>  
             </div>  
+            <div class="modal-body">  
+                <h4>Text in a modal</h4>  
+                <p>You can add some text here.</p>                
+            </div>  
+            <div class="modal-footer modalPink">  
+                <a href="#" class="btn btn-success">Call to action</a>  
+                <a href="#" class="btn" data-dismiss="modal">Close</a>  
+            </div>  
+        </div>  
 
             <!-- 
-                    Recommended Modal Division -->
+            Recommended Modal Division -->
 
             <div id="Recomended" class="modal hide fade in" style="display: none; ">  
                 <div class="modal-header modalPink modal-radius">  
@@ -292,6 +298,6 @@ include 'User.php';
             <script src="../js/js-script.js"></script>
             <script src="../js/bootstrap.min.js"></script>
 
-    </body>
+        </body>
 
-</html>
+        </html>
