a23a84477814b3439098c0908702f8e1e182296b
Shan Markus <shanmarkus@gmail.com>
2013-03-22 13:02:29 +0700

    Hardcoding User Profile
    Adding information for user profile for the sake of presentation

--- User/FoundationProfile.php
+++ User/FoundationProfile.php
@@ -17,121 +17,160 @@ $description=$foundationdata->getDescription();
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="../css/mainMenuCss.css">
-        <link rel="stylesheet" type="text/css" href="../css/FoundationProfile.css">
-        <title>Foundation Profile</title>
-    </head>
-    <body class="Background">
-               <div class="Header" >
-            <!-- Header class -->
-            <div class="row">
-                <div class="span12 margin-leftHeader margin-topHeader">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/mainMenuCss.css">
+    <link rel="stylesheet" type="text/css" href="../css/FoundationProfile.css">
+    <title>Foundation Profile</title>
+</head>
+<body class="Background">
+ <div class="Header" >
+    <!-- Header class -->
+    <div class="row">
+        <div class="span12 margin-leftHeader margin-topHeader">
 
-                    <div class="row-fluid">
-                        <div class="offset7 span4 margin-rightHeader">
-                            <?php include 'GlobalSearch.php' ?>
-                        </div>
+            <div class="row-fluid">
+                <div class="offset7 span4 margin-rightHeader">
+                    <?php include 'GlobalSearch.php' ?>
+                </div>
 
-                        <div class="span1">
-                            <?php include 'UserDropdownMenu.php'; ?>
-                        </div>
-                    </div>
-
+                <div class="span1">
+                    <?php include 'UserDropdownMenu.php'; ?>
                 </div>
             </div>
 
         </div>
+    </div>
 
-        <div class="row">
-            <div class="span3 Logo">
-                <!-- Logo Div  -->
+</div>
+
+<div class="row">
+    <div class="span3 Logo">
+        <!-- Logo Div  -->
+    </div>
+</div>
+
+<div class="row">
+
+    <div class="container">
+        <div class="row-fluid">
+            <div class="span2 paddingLeft">
+
+                <?php include('UserNavigation.php'); ?>
+
             </div>
-        </div>
-
-        <div class="row">
-
-            <div class="container">
+            <div class="span9">
                 <div class="row-fluid">
-                    <div class="span2 paddingLeft">
+                    <div class="foundationProfilePicture span2">
 
-                        <?php include('UserNavigation.php'); ?>
-
+                        <img style="border-radius:10px" src="../Database/Images/Foundation/<?php echo $images ?>"/>
+                        <p style="float:left;" ><font size="5"><?php echo $names; ?></font></p>
                     </div>
-                    <div class="span9">
+
+                    <div class="foundationProfilePost span9">
                         <div class="row-fluid">
-                            <div class="foundationProfilePicture span2">
 
-                                <img style="border-radius:10px" src="../Database/Images/Foundation/<?php echo $images ?>"/>
-                                <p style="float:left;" ><font size="5"><?php echo $names; ?></font></p>
-                            </div>
+                            <div class="span11 offset1">
+                                <h2><?php echo $names; ?></h2>
+                                <?php echo $city?> - 
+                                <?php echo $country?>
+                            </div>	
+                        </div>
+                    </div>
 
-                            <div class="foundationProfilePost span9">
-                                <div class="row-fluid">
+                </div>
 
-                                    <div class="span11 offset1">
-                                        <h2><?php echo $names; ?></h2>
-                                        <?php echo $city?> - 
-                                        <?php echo $country?>
-                                    </div>	
+                <div class="row-fluid margin-top">
+                    <!-- Main Menu Class -->
+                    <div class="span12">
+
+                        <div class="row-fluid">
+                            <div class="foundationProfileTop">
+                                <div class="myFont">
+                                    <p class="myFont"> Information </p>
                                 </div>
                             </div>
-
                         </div>
 
-                        <div class="row-fluid margin-top">
-                            <!-- Main Menu Class -->
-                            <div class="span12">
-
+                        <div class="row-fluid">
+                            <div class="foundationProfileMiddle">
                                 <div class="row-fluid">
-                                    <div class="foundationProfileTop">
-                                        <div class="myFont">
-                                            <p class="myFont"> Information </p>
-                                        </div>
-                                    </div>
+                                    <h4>Sejarah & Latar Belakang</h4>
+                                    <br>
+                                    <p>
+                                        Yayasan Lima Roti Dua Ikan Indonesia dimulai dari sebuah persekutuan doa yang berdiri pada sekitar bulan Oktober 2003, yang menamakan diri persekutuan doa Lima Roti Dua Ikan. Pendiri mendapat pewahyuan “ajaib” melalui firman Tuhan dalam Kisah Rasul pasal 6 pada tahun 1998. Dalam Kisah Rasul pasal 6 ada beberapa orang yang dipilih untuk melayani meja (handle finance, TEV) bagi para janda, dan oleh karenanya firman Allah semakin tersebar, jumlah murid bertambah dan sejumlah besar imam (hamba Tuhan) menyerahkan diri dan menjadi percaya. Walaupun arti dari firman itu pada saat itu belum sepenuhnya dimengerti, tetapi nantinya akan menjadi dasar acuan bagi pewahyuan-pewahyuan berikutnya yang diterima oleh pendiri sehingga yayasan pada akhirnya bisa terbentuk.
+                                        Pada bulan Februari 2004 pendiri kembali mendapat pewahyuan susulan melalui firman Tuhan (dalam suatu pergumulan doa di Taman Getsemani, Semarang). Tuhan memberikan ayat di 2 Raja-raja 4:42-44, dan melalui ayat ini pendiri diberikan konfirmasi untuk melayani Tuhan sepenuh waktu. Perintah Tuhan pada waktu itu adalah untuk memberi makan para abdi Allah, supaya mereka bisa melayani bangsa ini dengan maksimal. Tuhan meneguhkan perintah itu dengan memberikan kepada pendiri sebuah “tanda” konfirmasi yang masih disimpan pendiri sampai saat ini.
+                                    </p>
+                                </div>
+
+                                <div class="margin-top row-fluid">
+                                    <h4>Visi & Misi</h4>
+                                    <p>
+                                        VISI
+                                        <br>
+                                        Menjadi model lembaga pelayanan holistik pengentasan kemiskinan yang menghadirkan kerajaan Allah
+                                        <br><br>    
+                                        MISI
+                                        <br>
+                                        Melayani komunitas orang-orang tersisihkan secara holistik bermitra dengan “kaum Lewi”
+                                        <br><br>
+                                        MOTTO
+                                        <br>
+                                        Melayani Tuhan dengan memperhatikan sesama (We serve the Lord by serving others)
+
+
+                                    </p>
                                 </div>
 
                                 <div class="row-fluid">
-                                    <div class="foundationProfileMiddle">
-                                        <div class="row-fluid">
-                                            <h4>Where to Find ?</h4>
-                                            
-                                       
-                                        <?php echo $address." - ".$state ?><br/>
-                                        <?php echo $city." - ". $country?><br/>
-                                        Postal Code : <?php echo $postalCode;?></br>
-                                        Phone : <?php echo $phone?> <br/><br/>
-                                        <p ><?php echo $description?><br/></p>
-                                        </br>
-                                        <div class="row-fluid"> 
-                                        <form action="KidsCtrl.php" method="POST">
-                                            <input type="hidden" name="foundationid" value="<?php echo $foundationID?>"/>
-                                            <input type="hidden" name="action" value="listkidsbyfoundation"/>
-                                            <input type="submit" class="browseKids span2" value=""/>
-                                        </form>
-                                        </div>
-                                        
-                                     
-                                        <hr>
-                                        
-                                        
-               
-                                        <?php
-                                        if (count($foundationPost) > 0) {
-                                            for ($i = count($foundationPost) - 1; $i >= 0; $i--) {
-                                                $temp = $foundationPost[$i];
+                                   <h4> Q & A </h4>
+                                    What does the foundation do?
+                                    The Five Loaves and Two Fishes foundation is a non profit charitable organization that provide holistic charitable works for the poor and the needy. The people we serve is adult and children of all ages. Also we take care of the elderly and minister to the prisoners at prisons and other detention places.
+                                    How did it get started?
+                                    It started when Marko Budiman surrender his life to the Lord by gathering several of his friends to begin caring for the poor and the needy in 2004. Soon the number grew bigger and now they can accumulated enough money to do it in a regular basic. Soon the words spread out to other organizations and churches who loves their works for the poor and the needy, and they joined them. What began as a spark of flame has turned into a torch of love.
+                                    Where do the money go?
+                                    Since its beginning, the foundation has provided meals, basic needed goods to family shelters, and to numerous humanitarian organization around Jakarta and throughout Indonesia. We also sending our help to many of the disaster struck victims in Indonesia. All the proceed go to the most poor and the needy.
+
+                                </div>
+
+
+                                <div class="row-fluid">
+                                    <h4>Where to Find ?</h4>
+
+
+                                    <?php echo $address." - ".$state ?><br/>
+                                    <?php echo $city." - ". $country?><br/>
+                                    Postal Code : <?php echo $postalCode;?></br>
+                                    Phone : <?php echo $phone?> <br/><br/>
+                                    <p ><?php echo $description?><br/></p>
+                                </br>
+                                <div class="row-fluid"> 
+                                    <form action="KidsCtrl.php" method="POST">
+                                        <input type="hidden" name="foundationid" value="<?php echo $foundationID?>"/>
+                                        <input type="hidden" name="action" value="listkidsbyfoundation"/>
+                                        <input type="submit" class="browseKids span2" value=""/>
+                                    </form>
+                                </div>
+
+                                <hr>
+
+
+
+                                <?php
+                                if (count($foundationPost) > 0) {
+                                    for ($i = count($foundationPost) - 1; $i >= 0; $i--) {
+                                        $temp = $foundationPost[$i];
                                                 // echo $temp[0]."|". $temp[1]."|". $temp[2]."|". $temp[3]."|". $temp[4]."<br/>";
-                                                $id = $temp[0];
-                                                $title = $temp[1];
-                                                $photo = $temp[2];
-                                                $time = $temp[3];
-                                                $content = $temp[4];
-                                                ?>
-                                                <table style="width:100%"  >
-                                                    <tr>
-                                                        <td><div style="float:left"><a><?php echo $title; ?></a></div>
+                                        $id = $temp[0];
+                                        $title = $temp[1];
+                                        $photo = $temp[2];
+                                        $time = $temp[3];
+                                        $content = $temp[4];
+                                        ?>
+                                        <table style="width:100%"  >
+                                            <tr>
+                                                <td><div style="float:left"><a><?php echo $title; ?></a></div>
 
 <!--                                                            <form   onsubmit="return deletepost();" action="FoundationCtrl.php" method="post">
                                                                 <input type="hidden" name="foundationid" value="<?// echo $_SESSION['usernameF']; ?>"/>
@@ -149,34 +188,34 @@ $description=$foundationdata->getDescription();
                                                             <?php
                                                             if (!$photo == "") {
                                                                 ?>
-                                                        <img style=" max-height:150px;"  src='../Database/Images/Foundation/<?php echo $photo; ?>'/>
-                                                                                    
-                                                        <?php } ?>
-                                                 
-                                                   
-                                                    </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td> <?php echo $content; ?></td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td align="right"> 
-                                                            <font size="2"><?php echo $time; ?></font>
-                                                         
-                                                        </td>
-                                                    
-                                                    </tr>
+                                                                <img style=" max-height:150px;"  src='../Database/Images/Foundation/<?php echo $photo; ?>'/>
+
+                                                                <?php } ?>
 
 
-                                                </table>
-                                         <hr>
-                                                
-                                                <?php
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td> <?php echo $content; ?></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td align="right"> 
+                                                                <font size="2"><?php echo $time; ?></font>
+
+                                                            </td>
+
+                                                        </tr>
+
+
+                                                    </table>
+                                                    <hr>
+
+                                                    <?php
+                                                }
                                             }
-                                        }
-                                        ?>
+                                            ?>
                                         </div>
-                                       
+
                                     </div>
                                 </div>
 
@@ -192,30 +231,30 @@ $description=$foundationdata->getDescription();
                 </div>
             </div>
         </div>
-       
-
-
-                    <div class="Footer margin-top">
-                        <!-- footer div -->
-
-                    </div>
 
 
 
+        <div class="Footer margin-top">
+            <!-- footer div -->
 
-                    <!-- SCRIPT !!!  -->
-
-                    <script src="../js/jquery.js"></script>  
-                    <script src="../js/bootstrap-modal.js"></script>  
-                    <script src="../js/js-script.js"></script>
-                    <script src="../js/bootstrap.min.js"></script>
-
-
-
-                    <hr>
+        </div>
 
 
 
 
-                    </body>
-                    </html>
+        <!-- SCRIPT !!!  -->
+
+        <script src="../js/jquery.js"></script>  
+        <script src="../js/bootstrap-modal.js"></script>  
+        <script src="../js/js-script.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
+
+
+
+        <hr>
+
+
+
+
+    </body>
+    </html>
